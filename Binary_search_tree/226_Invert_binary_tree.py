7f6f3312158125cd70942c0dd6f163eea293e9ff
EvanNingduoZhao <ningduozhao@gmail.com>
2020-05-26 00:40:47 -0400

    add Problem 226 617

---
+++ Binary_search_tree/226_Invert_binary_tree.py
@@ -0,0 +1,21 @@
+class Solution:
+    #这是自己写的DFS的做法 关于DFS和BFS是什么区别以后学到了再补上
+    #这个方法其实就是preorder遍历，每access到一个node就把它的左右child调换
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        stack=[root]
+        while stack:
+            node=stack.pop()
+            if node:
+                temp=node.left
+                node.left=node.right
+                node.right=temp
+                stack.append(node.right)
+                stack.append(node.left)
+        return root
+
+    # recursively
+    def invertTree1(self, root):
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            #这个return放在if外面也是能跑过的
+            return root
